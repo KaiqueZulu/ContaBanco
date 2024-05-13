f9dfc2361de2a4a9dfa88921a5af652ed2f604f1
Kaique Pires <manozulukid@gmail.com>
2024-05-13 14:49:47 -0300

    first commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,10 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+    }
+    static class ContaBanco{
+        Integer numero;
+        String agencia, nomeCliente;
+        Float saldo;
+    }
+}
